246c867bac63edd53b3db395f72b64d697472e61
Muhamad Rizqi Ramdani <55569266+MuhamadRizqiRamdani@users.noreply.github.com>
2019-09-26 14:46:45 +0700

    Add files via upload

---
+++ src/main/java/com/mycompany/latihan1/Latihan1Biodata.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.latihan1;
+
+/**
+ *
+ * @author 
+ * NAMA     : Muhamad Rizqi Ramdani
+ * KELAS    : PBO2
+ * NIM      : 10118069
+ * Deskripsi program : Program ini berisi program untuk menampilkan biodata
+ * 
+ */
+public class Latihan1Biodata {
+    public static void main(String[] args) {
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "----------------");
+        System.out.println("NAMA\t\t: Muhamad Rizqi Ramdani");
+        System.out.println("ALAMAT\t\t: Jl. Kacapiring");
+        System.out.println("TEMPAT LAHIR\t: Bandung");
+        System.out.println("TANGGAL LAHIR\t: 10 Desember 2000");
+        System.out.println("-----------------------");
+        System.out.println("HOBI\t\t: Sepeda");
+        System.out.println("MAKES\t\t: Apa Aja Suka");
+        System.out.println("MIKES\t\t: Apa Aja Suka");
+    }   
+}
